234de447b05e6de5560729ac52a769dd30095b41
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-11-13 22:09:47 -0600

    bucket list item can be removed when clicked

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,14 +11,21 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
       // any to any transition
       transition('* => *', [
         query(':enter', style({ opacity: 0 }), {optional: true}),
+
         // stagger= delay time for when the element animates
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1})
-          ]))
-        ]), {optional: true})
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))]), {optional: true})
       ])
     ])
   ]
@@ -40,6 +47,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
