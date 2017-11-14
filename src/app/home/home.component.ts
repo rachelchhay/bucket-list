94ada57daf68d1c50e58cebe507fd3bd876f59d3
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-11-13 19:15:20 -0600

    Added animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('goals', [
+      // any to any transition
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        // stagger= delay time for when the element animates
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
